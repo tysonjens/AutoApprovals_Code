7909809bfb2e67f11a8ec7d19d695e945253438f
Tyson Ward <tysonjens@users.noreply.github.com>
2019-04-25 12:23:56 -0600

    Code to apply rules to newer data

--- referrals.sql
+++ referrals.sql
@@ -1,20 +1,21 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 SELECT rd.[PX_CODE] as CPT_Code
-      --,rh.[REF_TYPE_KEY]
+	  ,rh.[HCP_CONNECT_AUTH_NUMBER]
+      ,rh.[REF_TYPE_KEY]
       ,rh.[PPL]
       --,rh.[REFERRING_PROV_KEY]
       ,rh.[DT_RECEIVED] as Date_Received
-    ,rh.[DECISION_DT] as Date_Decision
+      ,rh.[DECISION_DT] as Date_Decision
       ,lrt.[REFERRAL_TYPE_NAME] as Type
       ,lrs.[STATUS_TYPE] as status_cat
-    ,lrs.[NAME] as status_name
+      ,lrs.[NAME] as status_name
       --,rtp.[NUMERIC_CODE]
       --,rtp.[NAME] as reftoprov
       --,lrf.[NAME] as refer_prov
       ,lus.[BILLING_AREA] as Specialty
       --,ls.[BUDGET_SITE] as Site
       ,ls.[BUDGET_COMMUNITY] as region
-    ,rd.[UNITS]
+      ,rd.[UNITS]
   
   FROM [IADS_V3].[dbo].[REFERRAL_DET] rd
     inner join [IADS_V3].[dbo].[REFERRAL_HDR] rh on rd.[REFERRAL_KEY] = rh.[REFERRAL_KEY]
@@ -26,7 +27,7 @@ SELECT rd.[PX_CODE] as CPT_Code
     inner join [IADS_V3].[dbo].[LU_REFERRAL_STATUS] lrs on rh.[REFERRAL_STATUS_KEY] = lrs.[Record_Number]
     inner join [IADS_V3].[dbo].[LU_REFERRAL_TYPE] lrt on rh.[REF_TYPE_KEY] = lrt.[REF_TYPE]
   WHERE
-    rh.[DT_RECEIVED] BETWEEN '2018-08-01' and '2018-11-01' 
+    rh.[DT_RECEIVED] BETWEEN '2019-01-15' and '2019-4-15' 
     --AND lrs.[STATUS_TYPE] IN ('APPROVED') 
     --AND lrt.[REFERRAL_TYPE_NAME] = 'PHYSICIAN'
   --AND rd.[PX_CODE] in ('99201', '99202', '99203', '99204', '99205', '99211', '99212', '99213', '99214', '99215')
