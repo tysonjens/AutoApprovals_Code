b1532ccf01814cf3a4bae3be6b44858338c26c80
tysonjens <tysonjens@gmail.com>
2019-07-10 21:30:06 -0600

    Add urgents

--- referrals.sql
+++ referrals.sql
@@ -9,6 +9,7 @@ SELECT rd.[PX_CODE] as CPT_Code
       ,lrs.[NAME] as status_name
       ,lus.[BILLING_AREA] as Specialty
       ,ls.[BUDGET_COMMUNITY] as region
+      ,lup.[NAME] as urgent
       ,rd.[UNITS]
       ,lbp.[PRODUCT_CATEGORY_NAME] as LOB
   FROM [IADS_V3].[dbo].[REFERRAL_DET] rd
@@ -16,6 +17,7 @@ SELECT rd.[PX_CODE] as CPT_Code
     inner join [IADS_V3].[dbo].[LU_REFERRING_PROVIDER] lrf on rh.[REFERRING_PROV_KEY] = lrf.[Record_Number]
     inner join [IADS_V3].[dbo].[LU_Specialty] lus on rh.[SPECIALTY_REF_TO_PROVIDER_KEY] = lus.[Record_Number]
     inner join [IADS_V3].[dbo].[LU_ELIGIBILITY] le on rh.[ELIG_KEY] = le.[ELIG_KEY]
+    inner join [IADS_V3].[dbo].[LU_PRIORITY] lup on rh.[PRIORITY_KEY] = lup.[Record_Number]
     inner join [IADS_V3].[dbo].[LU_SITE] ls on le.[HCP_SITE_KEY] = ls.[Record_Number]
     inner join [IADS_V3].[dbo].[LU_REFERRAL_STATUS] lrs on rh.[REFERRAL_STATUS_KEY] = lrs.[Record_Number]
     inner join [IADS_V3].[dbo].[LU_REFERRAL_TYPE] lrt on rh.[REF_TYPE_KEY] = lrt.[REF_TYPE]
